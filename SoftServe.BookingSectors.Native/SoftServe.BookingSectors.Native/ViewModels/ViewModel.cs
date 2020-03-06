67dacfd48cb23b5e3250013582c32401f2ace23b
vviniar <vviniar@softserveinc.com>
2020-03-06 11:34:55 +0200

    ViewModel base class

---
+++ SoftServe.BookingSectors.Native/SoftServe.BookingSectors.Native/ViewModels/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using SoftServe.BookingSectors.Native.Annotations;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace SoftServe.BookingSectors.Native.ViewModels
+{
+	/// <summary>
+	///  ViewModel base class
+	/// </summary>
+	public abstract class ViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		[NotifyPropertyChangedInvocator]
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		public void RaisePropertyChanged(params string[] propertyNames)
+		{
+			foreach (var propertyName in propertyNames)
+			{
+				PropertyChanged?.Invoke(this, new
+					PropertyChangedEventArgs(propertyName));
+			}
+		}
+
+		public INavigation Navigation { get; set; }
+	}
+}
