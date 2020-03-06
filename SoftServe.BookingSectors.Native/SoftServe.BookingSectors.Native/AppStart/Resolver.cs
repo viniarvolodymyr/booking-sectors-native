62dc6f924408d713c1aa3cca3770e10d5455fa1b
vviniar <vviniar@softserveinc.com>
2020-03-06 10:15:13 +0200

    Created Resolver.cs for creating our objects

---
+++ SoftServe.BookingSectors.Native/SoftServe.BookingSectors.Native/AppStart/Resolver.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+
+namespace SoftServe.BookingSectors.Native.AppStart
+{
+	/// <summary>
+	/// The resolver is responsible for creating our objects for us based on the type that we request
+	/// </summary>
+	public static class Resolver
+	{
+		private static IContainer _container;
+
+		public static void Initialize(IContainer container)
+		{
+			_container = container;
+		}
+
+		public static T Resolve<T>()
+		{
+			return _container.Resolve<T>();
+		}
+	}
+}
